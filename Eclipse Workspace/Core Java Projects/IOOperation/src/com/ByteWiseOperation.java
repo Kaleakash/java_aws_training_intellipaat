b533b022887fc8d8a368d8e75301ef6cf21fe271
akash <akash300383@gmail.com>
2022-11-23 10:08:57 +0530

    done

---
+++ Eclipse Workspace/Core Java Projects/IOOperation/src/com/ByteWiseOperation.java
@@ -0,0 +1,42 @@
+package com;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class ByteWiseOperation {
+
+	public static void main(String[] args) throws Exception{
+//		// source : keyboard and destination : console 
+//	DataInputStream dis = new DataInputStream(System.in);
+//	PrintStream ps = System.out;
+//	ps.println("Enter the name");
+//	String name = dis.readLine();
+//	ps.println("name is "+name);
+
+//		// source : keyboard and destination : file
+//		DataInputStream dis = new DataInputStream(System.in);
+//		//FileOutputStream fos = new FileOutputStream("abc.txt");		// data override 
+//		FileOutputStream fos = new FileOutputStream("abc.txt",true);		// append the data  
+//		System.out.println("enter the data");
+//		int ch;
+//		while((ch = dis.read())!= '\n') {
+//			fos.write(ch);
+//		}
+//		fos.close();
+		
+		// source : file and destination : file
+		FileInputStream fis = new FileInputStream("C:\\Users\\LENOVO\\OneDrive\\Desktop\\Maven Plugins.txt");
+		FileOutputStream fos = new FileOutputStream("info.txt",true);		// append the data  
+		int ch;
+		while((ch = fis.read())!= -1) {		// EOF in java -1
+			System.out.print((char)ch);	// we need to convert 
+			fos.write(ch);			// auto convert 
+		}
+		fis.close();		
+		fos.close();
+		System.out.println("copied");
+	}
+
+}
