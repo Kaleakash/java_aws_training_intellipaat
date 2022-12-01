2cf005c5d829b0c0d0f4f42a802826b1f0f1a459
akash <akash300383@gmail.com>
2022-12-01 10:46:00 +0530

    done

--- Maven Project/SimpleServletApp/src/main/java/com/Login.java
+++ Maven Project/SimpleServletApp/src/main/java/com/Login.java
@@ -3,6 +3,7 @@ package com;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,24 +30,30 @@ public class Login extends HttpServlet {
 		PrintWriter pw  = response.getWriter();
 		String emailid = request.getParameter("emailid");
 		String password = request.getParameter("password");
+		RequestDispatcher rd1 = request.getRequestDispatcher("Home");
+		RequestDispatcher rd2 = request.getRequestDispatcher("login.html");
+		response.setContentType("text/html");
 		if(emailid.equals("raj@gmail.com") && password.equals("123")) {
-			pw.println("successfully login with Get method");
+				pw.println("successfully login with Get method");
+			rd1.forward(request, response);			// we can see the output of only target page 
 		}else {
 			pw.println("failure try once again with get method");
+			rd2.include(request, response);				// source page + target page as one output 
 		}
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter pw  = response.getWriter();
 		String emailid = request.getParameter("emailid");
 		String password = request.getParameter("password");
+		RequestDispatcher rd1 = request.getRequestDispatcher("Home");
+		RequestDispatcher rd2 = request.getRequestDispatcher("login.html");
+		response.setContentType("text/html");
 		if(emailid.equals("raj@gmail.com") && password.equals("123")) {
 			pw.println("successfully login with post method");
+			rd1.forward(request, response);
 		}else {
 			pw.println("failure try once again with post method");
+			rd2.include(request, response);
 		}
 	}
 
