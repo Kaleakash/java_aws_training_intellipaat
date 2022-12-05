64460acaa6e3a5c29c43a60c32b0f7a0f65c845d
akash <akash300383@gmail.com>
2022-12-05 13:28:54 +0530

    done

--- Spring Framework/DIUsingXMLConfiguration/src/main/java/com/DemoTest.java
+++ Spring Framework/DIUsingXMLConfiguration/src/main/java/com/DemoTest.java
@@ -18,17 +18,26 @@ public class DemoTest {
 //	emp.display();
 		
 	ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
-	Employee e1 = (Employee)ac.getBean("emp1");
-	e1.display();
+//	Employee e1 = (Employee)ac.getBean("emp1");
+//	e1.display();
+//	
+//	Employee e2 = (Employee)ac.getBean("emp1");
+//	e2.display();
+//	
+//	Employee e3 = (Employee)ac.getBean("emp2");
+//	e3.display();
+//	
+//	Employee e4 = (Employee)ac.getBean("emp2");
+//	e4.display();
 	
-	Employee e2 = (Employee)ac.getBean("emp1");
-	e2.display();
-	
-	Employee e3 = (Employee)ac.getBean("emp2");
-	e3.display();
-	
-	Employee e4 = (Employee)ac.getBean("emp2");
-	e4.display();
+	Employee emp1 = (Employee)ac.getBean("emp1");
+	System.err.println(emp1);
+	Employee emp2 = (Employee)ac.getBean("emp2");
+	System.err.println(emp2);
+	Employee emp3 = (Employee)ac.getBean("emp3");
+	System.err.println(emp3);
+	Employee emp4 = (Employee)ac.getBean("emp4");
+	System.err.println(emp4);
 	}
 
 }
