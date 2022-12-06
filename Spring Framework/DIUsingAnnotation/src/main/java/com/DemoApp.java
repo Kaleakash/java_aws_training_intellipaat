9526b2e7a54c6f8a3485f7392a811150a0cdae15
akash <akash300383@gmail.com>
2022-12-06 11:30:12 +0530

    done

--- Spring Framework/DIUsingAnnotation/src/main/java/com/DemoApp.java
+++ Spring Framework/DIUsingAnnotation/src/main/java/com/DemoApp.java
@@ -1,12 +1,23 @@
 package com;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DemoApp {
 
 	public static void main(String[] args) {
-	ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+//	ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+//	Address add = (Address)ac.getBean("address");
+//	System.out.println(add);
+//	Employee emp = (Employee)ac.getBean("employee");
+//	System.out.println(emp);
+//	emp.setId(101);
+//	emp.setName("Ravi");
+//	emp.setSalary(12000);
+//	System.out.println(emp);
+		
+	AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(MyConfiguration.class);
 	Address add = (Address)ac.getBean("address");
 	System.out.println(add);
 	Employee emp = (Employee)ac.getBean("employee");
