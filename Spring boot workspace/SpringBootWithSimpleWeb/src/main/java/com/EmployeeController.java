159d80cfc338e7192f853b6a9fe27b38db6a6604
akash <akash300383@gmail.com>
2022-12-12 13:26:16 +0530

    done

--- Spring boot workspace/SpringBootWithSimpleWeb/src/main/java/com/EmployeeController.java
+++ Spring boot workspace/SpringBootWithSimpleWeb/src/main/java/com/EmployeeController.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -47,4 +49,44 @@ public class EmployeeController {
 //		listOfEmp.add(new Employee(3, "Ram", 16000));
 //		return listOfEmp;
 //	}
+	
+	
+	// http://localhost:8080/singleQuery?name=Ravi
+	@RequestMapping(value = "singleQuery",method = RequestMethod.GET)
+	public String singleQueryParam(@RequestParam("name") String uname) {
+		// we pass this value to service to dao layer. 
+		return "Welcome to spring boot with single query param "+uname;
+	}
+	
+	// http://localhost:8080/multiQuery?name=Ravi&pass=123
+	// http://localhost:8080/multiQuery?name=Ravi&pass=123456
+	
+		@RequestMapping(value = "multiQuery",method = RequestMethod.GET)
+		public String multiQueryParam(@RequestParam("name") String uname, @RequestParam("pass") String pass) {
+				if(uname.equals("Ravi") && pass.equals("123")) {
+					return "success using query param";
+				}else {
+					return "failure using query param";
+				}
+		}
+		
+	// http://localhost:8080/singlePath/Ravi
+		@RequestMapping(value = "singlePath/{name}",method = RequestMethod.GET)
+		public String singlePathParam(@PathVariable("name") String uname) {
+			// we pass this value to service to dao layer. 
+			return "Welcome to spring boot with single path param "+uname;
+	}
+	
+		// http://localhost:8080/multiPath/Ravi/123
+		// http://localhost:8080/multiPath/Ravi/123456
+				@RequestMapping(value = "multiPath/{name}/{pass}",method = RequestMethod.GET)
+				public String multiPathParam(@PathVariable("name") String uname, @PathVariable("pass") String pass) {
+					if(uname.equals("Ravi") && pass.equals("123")) {
+						return "success using path param";
+					}else {
+						return "failure using path param";
+					}	
+			}
+		
 }
+
